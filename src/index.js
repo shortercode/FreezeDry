7178b5126bd5f68ac560e98542192df7b34fa387
Iain Shorter <iainshorter@gmail.com>
2018-02-22 19:01:53 +0000

    Condense index mapping

--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
-import { encode } from "./encode.js";
-import { decode } from "./decode.js";
-
-/*
-	Designed to match the structured clone algorithm in behaviour,
-	with the exception of FileList objects as they cannot be created
-*/
-
-export encode;
-export decode;
+export { encode } from "./encode.js";
+export { decode } from "./decode.js";
