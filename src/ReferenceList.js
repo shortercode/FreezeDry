5af0ec39d3e04b3ecfee78a4f613088c16f5a6f4
shortercode <iainshorter@iains-mbp.home>
2018-02-25 22:48:22 +0000

    Corrected for invalid size calculation of REFERENCE tokens

--- src/ReferenceList.js
+++ src/ReferenceList.js
@@ -1,5 +1,23 @@
 import { REFERENCE } from "./types.js";
 
+const MAX_UINT = 2 ** 32;
+
+function vIntLength(obj) {
+	if (!Number.isInteger(obj) || obj >= MAX_UINT)
+		throw new Error("Invalid value for LEB UINT");
+	let length = 5;
+	if (obj < 128)
+		length =  1;
+	else if (obj < 16384)
+		length =  2;
+	else if (obj < 2097152)
+		length =  3;
+	else if (obj < 268435456)
+		length =  4;
+
+	return length;
+}
+
 export class ReferenceList {
 	constructor () {
 		this.data = new Map();
@@ -11,9 +29,13 @@ export class ReferenceList {
 		if (ref)
 			return ref;
 
+		const n = this.counter++;
+		const l = vIntLength(n);
+
 		this.data.set(obj, {
 			type: REFERENCE,
-			data: this.counter++
+			length: l,
+			data: n
 		});
 	}
 }
